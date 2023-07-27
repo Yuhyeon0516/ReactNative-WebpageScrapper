2f2159bd06c07faafd9e55258dea04ad5ee1a28a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-27 17:42:56 +0900

    add open graph data

--- src/screens/AddLinkScreen.js
+++ src/screens/AddLinkScreen.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, useWindowDimensions } from "react-native";
 import React, { useCallback, useState } from "react";
 import { Header } from "../components/Header/Header";
 import { useNavigation } from "@react-navigation/native";
@@ -10,12 +10,16 @@ import { Spacer } from "../components/Spacer";
 import { useSetRecoilState } from "recoil";
 import { atomLinkList } from "../states/atomLinkList";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getOpenGraphData } from "../util/OpenGraphTagUtils";
+import { RemoteImage } from "../components/RemoteImage";
 
 export default function AddLinkScreen() {
   const updateList = useSetRecoilState(atomLinkList);
   const safeAreaInset = useSafeAreaInsets();
   const navigation = useNavigation();
   const [url, setUrl] = useState("");
+  const [metaData, setMetaData] = useState(null);
+  const { width } = useWindowDimensions();
   const onPressClose = useCallback(() => {
     navigation.goBack();
   }, []);
@@ -41,6 +45,12 @@ export default function AddLinkScreen() {
     setUrl("");
   }, []);
 
+  const onSubmitEditing = useCallback(async () => {
+    const result = await getOpenGraphData(url);
+    console.log(result);
+    setMetaData(result);
+  }, [url]);
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
@@ -49,8 +59,26 @@ export default function AddLinkScreen() {
         </Header.Group>
         <Header.Icon iconName="close" onPress={onPressClose} />
       </Header>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingHorizontal: 24 }}>
-        <SingleLineInput value={url} onChangeText={setUrl} placeholder={"https://example.com"} />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-start", paddingTop: 32, paddingHorizontal: 24 }}>
+        <SingleLineInput value={url} onChangeText={setUrl} placeholder={"https://example.com"} onSubmitEditing={onSubmitEditing} />
+        {metaData && (
+          <>
+            <Spacer space={20} />
+            <View style={{ borderWidth: 1, borderRadius: 4, borderColor: "gray" }}>
+              <RemoteImage url={metaData.image} width={width - 48} height={(width - 48) * 0.5} />
+              <View style={{ paddingHorizontal: 12, paddingVertical: 8 }}>
+                <Spacer space={10} />
+                <Typography fontSize={20} color={"black"}>
+                  {metaData.title}
+                </Typography>
+                <Spacer space={4} />
+                <Typography fontSize={16} color={"gray"}>
+                  {metaData.description}
+                </Typography>
+              </View>
+            </View>
+          </>
+        )}
       </View>
       <CustomButton onPress={() => onPressSave(url)}>
         <View style={{ backgroundColor: !url ? "gray" : "black" }}>
