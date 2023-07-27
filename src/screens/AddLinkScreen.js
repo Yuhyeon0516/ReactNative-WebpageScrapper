2e516879566d8acf8fea03d6ec952574328b728a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-27 18:01:17 +0900

    Change ActivityIndicator to display while importing metadata

--- src/screens/AddLinkScreen.js
+++ src/screens/AddLinkScreen.js
@@ -1,4 +1,4 @@
-import { View, useWindowDimensions } from "react-native";
+import { ActivityIndicator, View, useWindowDimensions } from "react-native";
 import React, { useCallback, useEffect, useState } from "react";
 import { Header } from "../components/Header/Header";
 import { useNavigation } from "@react-navigation/native";
@@ -18,6 +18,7 @@ export default function AddLinkScreen() {
   const safeAreaInset = useSafeAreaInsets();
   const navigation = useNavigation();
   const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
   const [metaData, setMetaData] = useState(null);
   const { width } = useWindowDimensions();
   const onPressClose = useCallback(() => {
@@ -46,8 +47,10 @@ export default function AddLinkScreen() {
   }, []);
 
   const onSubmitEditing = useCallback(async () => {
+    setLoading(true);
     const result = await getOpenGraphData(url);
     setMetaData(result);
+    setLoading(false);
   }, [url]);
 
   const onGetClipBoardString = useCallback(async () => {
@@ -75,25 +78,38 @@ export default function AddLinkScreen() {
         </Header.Group>
         <Header.Icon iconName="close" onPress={onPressClose} />
       </Header>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-start", paddingTop: 32, paddingHorizontal: 24 }}>
+      <View style={{ flex: 1, justifyContent: "flex-start", paddingTop: 32, paddingHorizontal: 24 }}>
         <SingleLineInput value={url} onChangeText={setUrl} placeholder={"https://example.com"} onSubmitEditing={onSubmitEditing} />
-        {metaData && (
+        {loading ? (
           <>
             <Spacer space={20} />
             <View style={{ borderWidth: 1, borderRadius: 4, borderColor: "gray" }}>
-              <RemoteImage url={metaData.image} width={width - 48} height={(width - 48) * 0.5} />
-              <View style={{ paddingHorizontal: 12, paddingVertical: 8 }}>
-                <Spacer space={10} />
-                <Typography fontSize={20} color={"black"}>
-                  {metaData.title}
-                </Typography>
-                <Spacer space={4} />
-                <Typography fontSize={16} color={"gray"}>
-                  {metaData.description}
-                </Typography>
+              <Spacer space={(width - 48) * 0.5} />
+              <Spacer space={50} />
+              <View style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, alignItems: "center", justifyContent: "center" }}>
+                <ActivityIndicator />
               </View>
             </View>
           </>
+        ) : (
+          metaData && (
+            <>
+              <Spacer space={20} />
+              <View style={{ borderWidth: 1, borderRadius: 4, borderColor: "gray" }}>
+                <RemoteImage url={metaData.image} width={width - 48} height={(width - 48) * 0.5} />
+                <View style={{ paddingHorizontal: 12, paddingVertical: 8 }}>
+                  <Spacer space={10} />
+                  <Typography fontSize={20} color={"black"}>
+                    {metaData.title}
+                  </Typography>
+                  <Spacer space={4} />
+                  <Typography fontSize={16} color={"gray"}>
+                    {metaData.description}
+                  </Typography>
+                </View>
+              </View>
+            </>
+          )
         )}
       </View>
       <CustomButton onPress={() => onPressSave(url)}>
